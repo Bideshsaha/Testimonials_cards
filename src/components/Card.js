12a3e8dc387f1c22a4d72a41a0dddd889a7a004e
Bidesh <sahabidesh523@gmail.com>
2025-03-24 08:18:08 +0530

    app and card partial css added

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,9 +3,12 @@ import {FaQuoteLeft, FaQuoteRight} from 'react-icons/fa';
 const Card = (props) =>{
     let review = props.review;
     return(
-        <div>
-            <div>
-                <img src={review.image}/>
+        <div className="flex flex-col md:relative">
+            <div className="absolute top-[-7rem] z-[10] mx-auto">
+                <img 
+                className="aspect-squere rounded-full w-[140px] h-[140px] z-25"
+                src={review.image}/>
+                <div className="w-[140px] h-[140px] bg-violet-500 rounded-full absolute top-[-6px] z-[-10] left-[10px]"></div>
             </div>
             
             <div>
